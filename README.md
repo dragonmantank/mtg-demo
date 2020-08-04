667427486df5c4bc22bb4b22a5ca0a8d9a27c7d8
Kelly J Andrews <kelly@kellyjandrews.com>
2020-08-04 10:27:28 -0400

    scaffold frontend needs

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # mtg-demo
+
+## Frontend Build Directions
+
+The UI uses `create-react-app`.
+
+Use `npm start` in the `frontend` folder to run a development version.
+
+Use `npm run build` in the `frontend` folder to build a production version.
+The public folder is located at `frontend\public`.
---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider, CSSReset, theme } from '@chakra-ui/core';
+
+import App from './components/App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
